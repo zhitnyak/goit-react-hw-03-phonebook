712e8606030a09ec92b8b47037da9f87a405e4c1
Zhitnyak Maksim <m.zhitnyak@gmail.com>
2021-12-05 21:08:08 +0200

    copy project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
+import React, { Component } from "react";
 import "./App.css";
+import { nanoid } from "nanoid";
+import Form from "./components/Forma/Forma";
+import Filter from "./components/Filter/Filter";
+import ContactList from "./components/ContactList/ContactList";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const normalizeFilter = this.state.filter.toLowerCase();
+    return this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  addContact = ({ name, number }) => {
+    let unicName = this.state.contacts.find(
+      (contact) => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (unicName) {
+      alert(`${unicName.name} is already in contacts`);
+    } else {
+      // const userId = nanoid();
+      this.setState((prevState) => ({
+        contacts: [...prevState.contacts, { id: nanoid(), name, number }],
+      }));
+    }
+  };
+
+  deleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    const { filter } = this.state;
+    return (
+      <section className="wrapper">
+        <h1 className="title">Phonebook</h1>
+        <Form onSubmit={this.addContact} />
+        <h2 className="title">Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList contacts={visibleContacts} onChange={this.deleteContact} />
+      </section>
+    );
+  }
 }
 
 export default App;
